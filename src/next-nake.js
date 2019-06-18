59d1b6182572e50374087b8d31f93f947a2cd117
afeiship <1290657123@qq.com>
2019-06-19 00:36:48 +0800

    add: run method

--- src/next-nake.js
+++ src/next-nake.js
@@ -5,7 +5,29 @@
   var NxNake = nx.declare('nx.Nake', {
     statics: {
       __tasks__: {},
-      run: function() {},
+      run: function(inName) {
+        var start = Date.now();
+        var task = this.__tasks__[inName];
+        return task().then(
+          function(_) {
+            var end = Date.now();
+            var duration = end - start;
+            console.log(
+              '\x1b[36m%s\x1b[0m',
+              '[build]',
+              inName + ' done in ' + duration + 'ms'
+            );
+          },
+          function(err) {
+            console.error(
+              '\x1b[31m%s\x1b[0m',
+              '[build]',
+              'error running ' + inName + ':',
+              err.stack
+            );
+          }
+        );
+      },
       task: function(inPath, inTask) {
         nx.set(this.__tasks__, inPath, inTask);
       },
