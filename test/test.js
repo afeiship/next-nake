7c8b91ce6894ca67f91de787b9ff2cbff464b55b
afeiship <1290657123@qq.com>
2019-06-19 00:59:09 +0800

    add: run test

--- test/test.js
+++ test/test.js
@@ -1,17 +1,23 @@
-(function () {
+(function() {
   var nx = require('next-js-core2');
   var NxNake = require('../src/next-nake');
+  var fsExtra = require('fs-extra');
+  var fs = require('fs');
 
-  describe('NxNake.methods', function () {
-    test('init', function () {
-      var data = {
-        key: 1,
-        value: 2
-      };
+  describe('NxNake.methods', function() {
+    test('init', function() {
+      NxNake.task('clean', () => {
+        return fsExtra.remove('dist');
+      });
+
+      NxNake.task('copy', () => {
+        return fs.copy('./src/next-nake.js', './dist/next-nake.js');
+      });
+
+      NxNake.run('copy');
       // console.log(rs1, rs2);
 
       // expect(rs1).toBe(true);
-
     });
   });
-}());
+})();
